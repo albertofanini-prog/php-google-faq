f93aa776bf5c0af64a047c03020b8f0ae2ae3c07
albertofanini-prog <albertofanini@outlook.com>
2022-03-30 15:19:13 +0200

    2nd faq

--- index.php
+++ index.php
@@ -34,6 +34,20 @@
             Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.</p>
             
             '
+        ],
+        [
+            'ask' => '<h3>Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?</h3>',
+            'answer' => '<p>Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. 
+            Per noi è imperativo offrire un\'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</p>
+
+            <p>Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. 
+            Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. 
+            Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</p>
+
+            <p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.</p>
+
+            <p><a>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>
+            '
         ]
     ]
 ?>
