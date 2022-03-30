9826d039c918c945a1b0249911054e73717056a7
albertofanini-prog <albertofanini@outlook.com>
2022-03-30 14:26:19 +0200

    Header

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
 <?php
     var_dump('Google faq');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Domande Frequenti - Privacy e Termini</title>
+</head>
+<body>
+    <header>
+        <div class="toolbar">
+            <div class="logo">
+                <div class="image">
+                    <img src="images/google_logo.svg" alt="">
+                </div>
+                <div class="explain">
+                    <span><a class="privacy" href="#">Privacy e termini</a></span>
+                </div>
+            </div>
+            <div class="tools">
+                <div class="menu">
+                    <img src="images/menu.webp" alt="">
+                </div>
+                <div class="user">
+                <img src="images/user.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="navbar">
+
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
